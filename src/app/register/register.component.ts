6fe7c55087fa7390b64938a1929ad3fad764b32f
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-07 09:57:49 +0200

    ✨ Agregar imports de IonHeader, IonTitle e IonToolbar en el componente de registro

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {IonHeader, IonTitle, IonToolbar} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
+  imports: [
+    IonHeader,
+    IonTitle,
+    IonToolbar
+  ]
 })
 export class RegisterComponent  implements OnInit {
 
