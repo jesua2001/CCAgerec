7e4350b725ca275ab218a31add64e0f466b90429
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-07 11:46:43 +0200

    ✨ Añadimos el registrar para la conexion con el backend

--- src/app/core/servicies/auth.service.ts
+++ src/app/core/servicies/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '@environments/environment';
-import { LoginRequest, LoginResponse } from '@models/user.model';
+import {LoginRequest, LoginResponse, RegisterRequest} from '@models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -25,4 +25,19 @@ export class AuthService {
     const url = `${environment.apiBase}/${environment.endpoints.user}`;
     return this.http.post<LoginResponse>(url, formData, { params });
   }
+  register(data: RegisterRequest): Observable<RegisterRequest> {
+    const formData = new FormData();
+    formData.append('email', data.email);
+    formData.append('password', data.password);
+    formData.append('apellido1', data.apellido1);
+    formData.append('apellido2', data.apellido2);
+    formData.append('nombre',data.nombre);
+    const params = new HttpParams()
+      .set('action', 'registerUser')
+      .set('debug', '');
+
+    const url = `${environment.apiBase}/${environment.endpoints.user}`;
+    return this.http.post<RegisterRequest>(url, formData, { params });
+  }
+
 }
