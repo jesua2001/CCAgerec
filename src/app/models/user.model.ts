229d3cb913a602c6ae7c10a58f9be53f2dca89c2
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-07 11:47:56 +0200

    ✨ Actualizar modelos de usuario para incluir campos de registro y renombrar contraseña

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,11 +1,23 @@
 export interface LoginRequest {
   email: string;
-  contraseña: string;
+  password: string;
+
 
 }
 
 export interface LoginResponse {
-  token: string;
+  token?: string;
   success: string;
   error?: string;
+}
+
+
+export interface RegisterRequest {
+  email: string;
+  password: string;
+  apellido1:string,
+  apellido2:string,
+  isAdmin: boolean;
+  nombre: string;
+
 }
