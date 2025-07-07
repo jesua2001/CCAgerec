349b0c7026a9f50d0a55fd2743d5976bfc9b005e
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-07 15:31:58 +0200

    ♻️ Configurar entornos: añadir configuración de API y definir rutas de la aplicación

---
+++ src/app/app.routes.ts
@@ -0,0 +1,25 @@
+// app.routes.ts
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+export const AppRoutes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./register/register.component').then(m => m.RegisterComponent)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
