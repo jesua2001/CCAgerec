62d65b32776b1fde0b728ab7c84f1eac2c31b280
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-08 13:36:01 +0200

    ✨anadir las rutas para developer y admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,29 @@
-// app.routes.ts
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 
-export const AppRoutes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
-  },
-  {
-    path: 'login',
-    loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)
-  },
-  {
-    path: 'register',
-    loadComponent: () => import('./register/register.component').then(m => m.RegisterComponent)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  }
-];
+  import { Routes } from '@angular/router';
+  import { HomeComponent } from './home/home.component';
+  import { LoginComponent } from './login/login.component';
+  import { RegisterComponent } from './register/register.component';
+
+  export const AppRoutes: Routes = [
+    {
+      path: 'home',
+      loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+    },
+    {
+      path: 'login',
+      loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)
+    },
+    {
+      path: 'register',
+      loadComponent: () => import('./register/register.component').then(m => m.RegisterComponent)
+    },
+    {
+      path: '',
+      redirectTo: 'home',
+      pathMatch: 'full'
+    },
+    {
+      path: 'developer-tools',
+      loadComponent: () => import('./developertools/developertools.component').then(m => m.DeveloperPanelPage)
+    }
+  ];
