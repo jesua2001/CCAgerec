aba1903c5e32b7d9809f7d641bc1f9d569201ae4
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-08 09:45:13 +0200

    🔥 Eliminar metodo inecesario

--- src/servicies/api.service.ts
+++ src/servicies/api.service.ts
@@ -5,14 +5,10 @@ import { HttpClient } from '@angular/common/http';
 export class ApiService {
   private baseUrl = 'http://localhost:8000/api';
 
-  // eslint-disable-next-line @angular-eslint/prefer-inject
   constructor(private http: HttpClient) {}
 
   login(datos: any) {
     return this.http.post(`${this.baseUrl}/login.php`, datos);
   }
 
-  obtenerProductos() {
-    return this.http.get(`${this.baseUrl}/productos.php`);
-  }
 }
