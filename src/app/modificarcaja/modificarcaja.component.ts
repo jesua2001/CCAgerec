ffb139cd053caaf275ae2e58987e97d0717f8f86
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-09 12:57:41 +0200

    🔥 Eliminar el archivo de estilos SCSS del componente Modificarcaja

--- src/app/modificarcaja/modificarcaja.component.ts
+++ src/app/modificarcaja/modificarcaja.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-modificarcaja',
   templateUrl: './modificarcaja.component.html',
-  styleUrls: ['./modificarcaja.component.scss'],
   standalone: true,
 })
 export class ModificarcajaComponent  implements OnInit {
