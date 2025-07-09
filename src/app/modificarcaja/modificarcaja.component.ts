39a9013daae5093f6288c975a23a80c721340bf9
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-09 12:55:34 +0200

    ✨ Añadir el componente Modificarcaja con su plantilla, estilos y pruebas unitarias

---
+++ src/app/modificarcaja/modificarcaja.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modificarcaja',
+  templateUrl: './modificarcaja.component.html',
+  styleUrls: ['./modificarcaja.component.scss'],
+  standalone: true,
+})
+export class ModificarcajaComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
