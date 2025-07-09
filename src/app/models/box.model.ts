ec4dd1894d494d6779fbfab77e3335db91694c52
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-09 10:29:16 +0200

    ✨ Realizacion y creacion del modelo de consulta

--- src/app/models/box.model.ts
+++ src/app/models/box.model.ts
@@ -1,6 +1,11 @@
 export interface BoxObtener {
   NSERIE: string;
   caneth: string;
+  certificado: string;
+  ipinterno: string;
+  ipredmaquina: string;
+  diagnostico: string;
+
 }
 
 export interface BoxAnadir {
