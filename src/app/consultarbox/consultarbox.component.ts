9e5d2ebd047db3cddfe69fbe0b18f2c105b1c498
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-09 08:58:38 +0200

    💄Añadir el app header para que se vea bien

--- src/app/consultarbox/consultarbox.component.ts
+++ src/app/consultarbox/consultarbox.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {HeaderComponent} from "../header/header.component";
 
 @Component({
   selector: 'app-consultarbox',
   templateUrl: './consultarbox.component.html',
   standalone: true,
+  imports: [
+    HeaderComponent
+  ]
 })
 export class ConsultarboxComponent  implements OnInit {
 
