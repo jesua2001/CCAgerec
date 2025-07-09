07d1a98145a5e785714d9c68c05f86a4c290c552
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-09 10:25:11 +0200

    ✨ Agregar funcionalidad para consultar la caja

--- src/app/consultarbox/consultarbox.component.ts
+++ src/app/consultarbox/consultarbox.component.ts
@@ -1,18 +1,56 @@
 import { Component, OnInit } from '@angular/core';
-import {HeaderComponent} from "../header/header.component";
+import { HeaderComponent } from "../header/header.component";
+import { IonButton, IonContent, IonInput, IonItem, IonLabel } from "@ionic/angular/standalone";
+import { BoxService } from "../core/servicies/box.service";
+import { BoxObtener } from "../models/box.model";
+import {FormsModule} from "@angular/forms";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-consultarbox',
   templateUrl: './consultarbox.component.html',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    CommonModule,
+    IonContent,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButton,
+    FormsModule
   ]
 })
-export class ConsultarboxComponent  implements OnInit {
+export class ConsultarboxComponent implements OnInit {
 
-  constructor() { }
+  numeroSerie: string = '';
+  mensajeError: string = '';
+  informationCaja: BoxObtener[] = [];
 
-  ngOnInit() {}
+  constructor(private boxService: BoxService) {
+  }
 
+  ngOnInit() {
+  }
+
+  consultarCaja() {
+    if (!this.numeroSerie.trim()) {
+      this.mensajeError = 'El número de serie no puede estar vacío.';
+      return;
+    }
+
+    this.boxService.getcaneth(this.numeroSerie).subscribe({
+      next: (response) => {
+        this.informationCaja = response;
+        this.mensajeError = '';
+      },
+      error: () => {
+        this.mensajeError = 'No se pudo obtener la IP de la caja.';
+
+        this.informationCaja = [];
+      }
+    });
+
+
+  }
 }
