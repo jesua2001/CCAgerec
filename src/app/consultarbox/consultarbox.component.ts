c738b0cc0bb975db2d72438d19683848c63c9360
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-09 08:12:31 +0200

    🔥 Eliminar el syle sheet del component ts

--- src/app/consultarbox/consultarbox.component.ts
+++ src/app/consultarbox/consultarbox.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-consultarbox',
   templateUrl: './consultarbox.component.html',
-  styleUrls: ['./consultarbox.component.scss'],
   standalone: true,
 })
 export class ConsultarboxComponent  implements OnInit {
