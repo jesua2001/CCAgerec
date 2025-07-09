8277588121cc3c5dbebb7ddd30180a09fa8fe049
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-09 11:45:17 +0200

    🔥 Eliminar el enlace del scss eliminado anteriormente

--- src/app/anadirbox/anadirbox.component.ts
+++ src/app/anadirbox/anadirbox.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-anadirbox',
   templateUrl: './anadirbox.component.html',
-  styleUrls: ['./anadirbox.component.scss'],
   standalone: true,
 })
 export class AnadirboxComponent  implements OnInit {
