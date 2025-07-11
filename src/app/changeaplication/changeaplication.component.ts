51e802d7d3019033daa0540d93bf71581aeb6018
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-11 11:07:21 +0200

    🔥 remover el enlace del scss con el component

--- src/app/changeaplication/changeaplication.component.ts
+++ src/app/changeaplication/changeaplication.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-changeaplication',
   templateUrl: './changeaplication.component.html',
-  styleUrls: ['./changeaplication.component.scss'],
   standalone: true,
 })
 export class ChangeaplicationComponent  implements OnInit {
