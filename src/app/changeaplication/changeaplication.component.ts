846f4ca6bdf38f0c5b52030c15605d62aaeac74c
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-11 11:08:35 +0200

    ✨ Reemplazar contenido del componente Changeaplication con el componente Header

--- src/app/changeaplication/changeaplication.component.ts
+++ src/app/changeaplication/changeaplication.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {HeaderComponent} from "../header/header.component";
 
 @Component({
   selector: 'app-changeaplication',
   templateUrl: './changeaplication.component.html',
   standalone: true,
+  imports: [
+    HeaderComponent
+  ]
 })
 export class ChangeaplicationComponent  implements OnInit {
 
