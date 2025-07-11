a66348dc36827226fc58e36ca0cc2fb9396d6e54
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-11 11:59:56 +0200

    ✨ Añadir navegación y botones al componente Changeaplication

--- src/app/changeaplication/changeaplication.component.ts
+++ src/app/changeaplication/changeaplication.component.ts
@@ -1,18 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import {HeaderComponent} from "../header/header.component";
+import {IonButton, IonContent} from "@ionic/angular/standalone";
+import {windows} from "rimraf";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-changeaplication',
   templateUrl: './changeaplication.component.html',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    IonContent,
+    IonButton,
   ]
 })
 export class ChangeaplicationComponent  implements OnInit {
 
-  constructor() { }
+  constructor (private router: Router) {}
 
   ngOnInit() {}
+
+  changeApplicationACC() {
+    this.router.navigate(['/home']);
+  }
+
+  changeApplicationVG() {
+    this.router.navigate(['/changeapplicationvg']);
+  }
+
+
 
 }
