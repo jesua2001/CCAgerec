f91d957cf7d7ae1fb570b1d4631a861ee1a07d16
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-11 11:06:11 +0200

    ✨ Añadir componente Changeaplication con su plantilla y estilos

---
+++ src/app/changeaplication/changeaplication.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-changeaplication',
+  templateUrl: './changeaplication.component.html',
+  styleUrls: ['./changeaplication.component.scss'],
+  standalone: true,
+})
+export class ChangeaplicationComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
