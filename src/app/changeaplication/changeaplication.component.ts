808e1a13b64de9987d178318b5199ecd89b20856
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-14 08:30:32 +0200

    ✨ Añadir lógica de visualización condicional para botones de cambio de aplicación según roles de usuario

--- src/app/changeaplication/changeaplication.component.ts
+++ src/app/changeaplication/changeaplication.component.ts
@@ -1,33 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import {HeaderComponent} from "../header/header.component";
-import {IonButton, IonContent} from "@ionic/angular/standalone";
-import {windows} from "rimraf";
-import {Router} from "@angular/router";
+import { HeaderComponent } from "../header/header.component";
+import { IonButton, IonContent } from "@ionic/angular/standalone";
+import { Router } from "@angular/router";
+import {AuthService} from "../core/servicies/auth.service";
+import {NgIf} from "@angular/common";
+
+
 
 @Component({
   selector: 'app-changeaplication',
   templateUrl: './changeaplication.component.html',
   standalone: true,
   imports: [
+
     HeaderComponent,
     IonContent,
     IonButton,
+    NgIf
   ]
 })
-export class ChangeaplicationComponent  implements OnInit {
+export class ChangeaplicationComponent implements OnInit {
 
-  constructor (private router: Router) {}
+  showCcAgerec = false;
+  showVgServicies = false;
 
-  ngOnInit() {}
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit() {
+    const isAdmin = this.authService.isAdmin();
+    this.showCcAgerec = isAdmin || this.authService.isCcAgerec();
+    this.showVgServicies = isAdmin || this.authService.isVgservicies();
+  }
 
   changeApplicationACC() {
-    this.router.navigate(['/home']);
+    this.router.navigate(['/home']);  // ruta para ccagerec
   }
 
   changeApplicationVG() {
-    this.router.navigate(['/changeapplicationvg']);
+    this.router.navigate(['/changeapplicationvg']);  // ruta para vgservicies
   }
-
-
-
 }
