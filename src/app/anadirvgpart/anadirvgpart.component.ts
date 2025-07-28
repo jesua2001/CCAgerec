eb3da8e3e9847b0d39b7742b8786e967fb718259
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-28 13:44:05 +0200

    🔥 Elimino el archivo de estilos SCSS del componente anadirvgpart

--- src/app/anadirvgpart/anadirvgpart.component.ts
+++ src/app/anadirvgpart/anadirvgpart.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-anadirvgpart',
   templateUrl: './anadirvgpart.component.html',
-  styleUrls: ['./anadirvgpart.component.scss'],
   standalone: true,
 })
 export class AnadirvgpartComponent  implements OnInit {
