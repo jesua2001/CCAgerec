d32e8275589ffa6f9e1548b377b97fe891ad6987
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-28 11:16:09 +0200

    🔥 Eliminar archivo SCSS del componente homevgpart

--- src/app/homevgpart/homevgpart.component.ts
+++ src/app/homevgpart/homevgpart.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-homevgpart',
   templateUrl: './homevgpart.component.html',
-  styleUrls: ['./homevgpart.component.scss'],
   standalone: true,
 })
 export class HomevgpartComponent  implements OnInit {
