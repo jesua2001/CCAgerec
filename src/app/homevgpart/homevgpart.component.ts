0e33f54e4a60945044c10f0ab1f33e5d2f233bf1
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-28 11:36:58 +0200

    ✨ Importar el componente Header en homevgpart

--- src/app/homevgpart/homevgpart.component.ts
+++ src/app/homevgpart/homevgpart.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {HeaderComponent} from "../header/header.component";
 
 @Component({
   selector: 'app-homevgpart',
   templateUrl: './homevgpart.component.html',
   standalone: true,
+  imports: [
+    HeaderComponent
+  ]
 })
 export class HomevgpartComponent  implements OnInit {
 
