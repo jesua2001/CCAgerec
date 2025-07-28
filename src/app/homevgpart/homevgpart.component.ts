a7929115fa772a995414cf9fd538dc7678c61eb8
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-28 11:53:18 +0200

    ✨ Importar componentes de Ionic y agregar método consultarHomevgparts en homevgpart

--- src/app/homevgpart/homevgpart.component.ts
+++ src/app/homevgpart/homevgpart.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import {HeaderComponent} from "../header/header.component";
+import {IonButton, IonContent, IonInput, IonItem, IonLabel} from "@ionic/angular/standalone";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-homevgpart',
   templateUrl: './homevgpart.component.html',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    IonButton,
+    IonContent,
+    IonInput,
+    IonItem,
+    IonLabel,
+    NgForOf,
+    NgIf
   ]
 })
 export class HomevgpartComponent  implements OnInit {
@@ -15,4 +24,7 @@ export class HomevgpartComponent  implements OnInit {
 
   ngOnInit() {}
 
+  consultarHomevgparts() {
+
+  }
 }
