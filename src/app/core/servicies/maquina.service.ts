b263e9d1d584afcea1c0a1db6e19be3f16d8db68
Jesuarinconero <117852917+Jesuarinconero@users.noreply.github.com>
2025-07-30 11:01:22 +0200

    ✨ Realizacion del servicio para transmitir la inforamcion del backend al frontend

---
+++ src/app/core/servicies/maquina.service.ts
@@ -0,0 +1,31 @@
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Maquina} from "@models/maquina.model";
+import {Injectable} from "@angular/core";
+import {environment} from "@environments/environment";
+import * as url from "node:url";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class MaquinaService {
+
+  constructor(private http: HttpClient) {}
+
+  obtenerEquivalencia(modelo: string, marca: string, numeroSerie: string): Observable<Maquina[]> {
+    const formData = new FormData();
+    formData.append('MODELO', modelo);
+    formData.append('MARCA', marca);
+    formData.append('SERIE', numeroSerie);
+
+    const params = new HttpParams()
+      .set('action', 'obtenerCruceApiladores')
+      .set('debug', '');
+
+    const url = `${environment.apiBase}/${environment.endpoints.maquina}`;
+    return this.http.post<Maquina[]>(url, formData, { params });
+  }
+
+
+}
