2caf628fb6734118fa9a8d350d46af6388655910
Manuel Calderón Castaño <manolo24calder@gmail.com>
2025-07-31 07:56:18 +0200

    ✨ Creacion del componente

---
+++ src/app/anadirvgpartceexistente/anadirvgpartceexistente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anadirvgpartceexistente',
+  templateUrl: './anadirvgpartceexistente.component.html',
+  styleUrls: ['./anadirvgpartceexistente.component.scss'],
+  standalone: true,
+})
+export class AnadirvgpartceexistenteComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
