dbedfedaa2cad634e0784bc6675a0d392ede6cb6
Manuel Calderón Castaño <manolo24calder@gmail.com>
2025-07-31 07:57:41 +0200

    🔥 Elimino codigo del scss en el componente ts

--- src/app/anadirvgpartceexistente/anadirvgpartceexistente.component.ts
+++ src/app/anadirvgpartceexistente/anadirvgpartceexistente.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-anadirvgpartceexistente',
   templateUrl: './anadirvgpartceexistente.component.html',
-  styleUrls: ['./anadirvgpartceexistente.component.scss'],
   standalone: true,
 })
 export class AnadirvgpartceexistenteComponent  implements OnInit {
